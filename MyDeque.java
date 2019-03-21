e50d5bcd2eec69898b960b61335652d2dd02202a
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-03-21 11:19:45 -0400

    maybe last commit of class

--- MyDeque.java
+++ MyDeque.java
@@ -1,14 +1,21 @@
 public class MyDeque<E>{
   private E[] data;
-  private int size, start, end;
+  private int size, start, end; //Start as 0 because of int default
 
   @SuppressWarnings("unchecked")
   public MyDeque(){
     data = (E[])new Object[10];
+
+  }
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity];
+  }
+
+  public int size(){
+    return size;
   }
   /*
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
   public String toString(){ }
   public void addFirst(E element){ }
   public void addLast(E element){ }
