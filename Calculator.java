fbacf24ac7095f8662dca13d02d1c5a81df50eb8
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-03-26 10:48:28 -0400

    messing with main, added modulo

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,24 @@
 public class Calculator{
-    /*Evaluate a postfix expression stored in s.
-     *Assume valid postfix notation, separated by spaces.
-     */
-    public static double eval(String s){
-      String[] vals = s.split(" ");
-      MyDeque<Double> stack = new MyDeque<Double>();
-      for(String x:vals){
-        if(x=="+") stack.addLast(stack.removeLast()+stack.removeLast());
-        else if(x=="-") stack.addLast(0-stack.removeLast()+stack.removeLast());
-        else if(x=="*") stack.addLast(stack.removeLast()*stack.removeLast());
-        else if(x=="/") stack.addLast(1/stack.removeLast()*stack.removeLast());
-        else stack.addLast(Double.parseDouble(x));
+  public static void main(String[]args){
+    System.out.println(eval(args[0]));
+  }
+  /*Evaluate a postfix expression stored in s.
+  *Assume valid postfix notation, separated by spaces.
+  */
+  public static double eval(String s){
+    String[] vals = s.split(" ");
+    MyDeque<Double> stack = new MyDeque<Double>();
+    for(String x:vals){
+      if(x=="+") stack.addLast(stack.removeLast()+stack.removeLast());
+      else if(x=="-") stack.addLast(0-stack.removeLast()+stack.removeLast());
+      else if(x=="*") stack.addLast(stack.removeLast()*stack.removeLast());
+      else if(x=="/") stack.addLast(1/stack.removeLast()*stack.removeLast());
+      else if(x=="%"){
+        Double last = stack.removeLast();
+        stack.addLast(stack.removeLast()%last);
       }
-      return stack.getLast();
+      else stack.addLast(Double.parseDouble(x));
     }
+    return stack.getLast();
+  }
 }
